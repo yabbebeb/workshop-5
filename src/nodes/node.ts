024e2ad0ee8414e66136e05d1989f9561a45ad9a
yabbebeb <adrx@MSIad>
2024-03-18 13:09:17 +0100

    mise a jour

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -1,7 +1,7 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { BASE_NODE_PORT } from "../config";
-import { Value } from "../types";
+import { Value, NodeState } from "../types";
 
 export async function node(
   nodeId: number, // the ID of the node
@@ -15,14 +15,26 @@ export async function node(
   const node = express();
   node.use(express.json());
   node.use(bodyParser.json());
+  let state: NodeState = {
+    killed: false,
+    x: isFaulty ? null : initialValue,
+    decided: isFaulty ? null : false,
+    k: isFaulty ? null : 0,
+  };
 
-  // TODO implement this
-  // this route allows retrieving the current status of the node
-  // node.get("/status", (req, res) => {});
+  // Respond with the current status of the node
+  node.get("/status", (req, res) => {
+    if (isFaulty) {
+      res.status(500).send("faulty");
+    } else {
+      res.status(200).send("live");
+    }
+  });
 
-  // TODO implement this
-  // this route allows the node to receive messages from other nodes
-  // node.post("/message", (req, res) => {});
+  // Get the current state of a node
+  node.get("/getState", (req, res) => {
+    res.json(state);
+  });
 
   // TODO implement this
   // this route is used to start the consensus algorithm
